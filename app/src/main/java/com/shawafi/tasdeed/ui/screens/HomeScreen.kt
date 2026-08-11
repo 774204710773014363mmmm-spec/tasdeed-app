e2222088c62fec7586926872d116e1b74871920d
774204710773014363mmmm-spec <774204710773014363mmmm@gmail.com>
2026-08-12 01:44:20 +0300

    fix app/src/main/java/com/shawafi/tasdeed/ui/screens/HomeScreen.kt

--- app/src/main/java/com/shawafi/tasdeed/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/shawafi/tasdeed/ui/screens/HomeScreen.kt
@@ -8,9 +8,9 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Lock
 import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
