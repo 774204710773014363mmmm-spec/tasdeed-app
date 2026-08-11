57e74d928df9ede158ab3544297412e7daf7bbc1
774204710773014363mmmm-spec <774204710773014363mmmm@gmail.com>
2026-08-12 00:26:13 +0300

    add www/sw.js

---
+++ www/sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'tasdeed-v1';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
