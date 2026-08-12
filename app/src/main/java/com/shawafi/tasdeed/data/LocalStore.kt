e990264a25d791df7a0167a1e125a846fa9efb6e
774204710773014363mmmm-spec <774204710773014363mmmm@gmail.com>
2026-08-13 02:17:05 +0300

    update app/src/main/java/com/shawafi/tasdeed/data/LocalStore.kt

--- app/src/main/java/com/shawafi/tasdeed/data/LocalStore.kt
+++ app/src/main/java/com/shawafi/tasdeed/data/LocalStore.kt
@@ -146,44 +146,4 @@ class LocalStore(context: Context) {
         }
         return out
     }
-
-    // ---------- ميزة فواتير SMS ----------
-
-    fun saveSmsRows(list: List<SmsRow>) {
-        val arr = JSONArray()
-        list.forEach { arr.put(it.toJson()) }
-        putJson("sms_rows", arr)
-    }
-
-    fun loadSmsRows(): MutableList<SmsRow> {
-        val out = mutableListOf<SmsRow>()
-        getJsonArray("sms_rows")?.let { arr ->
-            for (i in 0 until arr.length()) {
-                arr.optJSONObject(i)?.let { out.add(SmsRow.from(it)) }
-            }
-        }
-        return out
-    }
-
-    fun saveSmsHistory(list: List<SmsHistoryEntry>) {
-        val arr = JSONArray()
-        list.forEach { arr.put(it.toJson()) }
-        putJson("sms_history", arr)
-    }
-
-    fun loadSmsHistory(): MutableList<SmsHistoryEntry> {
-        val out = mutableListOf<SmsHistoryEntry>()
-        getJsonArray("sms_history")?.let { arr ->
-            for (i in 0 until arr.length()) {
-                arr.optJSONObject(i)?.let { out.add(SmsHistoryEntry.from(it)) }
-            }
-        }
-        return out
-    }
-
-    fun saveSmsSettings(s: SmsSettings) {
-        putJson("sms_settings", s.toJson())
-    }
-
-    fun loadSmsSettings(): SmsSettings = SmsSettings.from(getJsonObject("sms_settings"))
 }
