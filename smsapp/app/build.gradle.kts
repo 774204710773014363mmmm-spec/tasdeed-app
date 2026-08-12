77f57ccad31396f9c7beefa69e9d246491f5fe00
774204710773014363mmmm-spec <774204710773014363mmmm@gmail.com>
2026-08-13 02:59:03 +0300

    update smsapp smsapp/app/build.gradle.kts

--- smsapp/app/build.gradle.kts
+++ smsapp/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.shawafi.smsapp"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
+        versionCode = 8
         versionName = "1.0"
     }
 
