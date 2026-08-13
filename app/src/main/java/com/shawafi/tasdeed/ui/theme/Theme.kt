35ca8e0c3419c165542a9c2414fb3ef55944f3f7
774204710773014363mmmm-spec <774204710773014363mmmm@gmail.com>
2026-08-13 03:55:40 +0300

    update app/src/main/java/com/shawafi/tasdeed/ui/theme/Theme.kt

--- app/src/main/java/com/shawafi/tasdeed/ui/theme/Theme.kt
+++ app/src/main/java/com/shawafi/tasdeed/ui/theme/Theme.kt
@@ -65,11 +65,11 @@ private val DarkColors = darkColorScheme(
 )
 
 private val AppShapes = Shapes(
-    extraSmall = RoundedCornerShape(8.dp),
-    small = RoundedCornerShape(12.dp),
-    medium = RoundedCornerShape(16.dp),
-    large = RoundedCornerShape(20.dp),
-    extraLarge = RoundedCornerShape(28.dp)
+    extraSmall = RoundedCornerShape(10.dp),
+    small = RoundedCornerShape(14.dp),
+    medium = RoundedCornerShape(18.dp),
+    large = RoundedCornerShape(24.dp),
+    extraLarge = RoundedCornerShape(32.dp)
 )
 
 @Composable
