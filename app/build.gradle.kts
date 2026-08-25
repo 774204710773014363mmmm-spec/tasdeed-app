8e21fbb90cad12a09e2c337b8dd80fa2f82e4e9e
774204710773014363mmmm-spec <774204710773014363mmmm@gmail.com>
2026-08-25 21:01:14 +0300

    update app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.shawafi.tasdeed"
         minSdk = 24
         targetSdk = 35
-        versionCode = 67
+        versionCode = 68
         versionName = "1.0"
     }
 
